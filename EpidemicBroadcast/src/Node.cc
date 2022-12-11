d4593965ab31a149d44a2ee1dc98917e4ee9d53c
Guillaume Quint <g.quint@studenti.unipi.it>
2022-12-11 13:18:59 +0100

    add stats neighbors, endTime, hop

--- EpidemicBroadcast/src/Node.cc
+++ EpidemicBroadcast/src/Node.cc
@@ -19,20 +19,22 @@
 #include <numeric>
 Define_Module(Node);
 
+# define INFECTION_FRMT "Infetto %d"
+
 void Node::sendAll(){
 
     int n = par("numberOfNodes");
     for(int i = 0; i<n; ++i ){
        char str[20];
-       sprintf(str, "Infetto %d", self_id);
+       sprintf(str, INFECTION_FRMT, infectionHop);
 
         if(isReachable[i]){
             cMessage * mess = new cMessage(str);
             send(mess,"out",i);
         }
+        EV<<"INVIATO "<<str<<endl;
     }
     hasInfected=true;
-    EV<<"INVIATO "<<self_id<<endl;
     colorNode((char*)"red");
 }
 
@@ -71,17 +73,28 @@ void Node::initialize()
         else
             connDispStr.parse("ls=red,0;");
     }
-    int sum = 0;
+    neighborsSignal = registerSignal("neighbors");
+    endTimeSignal = registerSignal("endTime");
+    collisionSignal = registerSignal("collision");
+    hopSignal = registerSignal("hop");
+
+    int sumNeighbors = 0;
     for (int i = 0; i < numberOfNodes; ++i){
-        sum += isReachable[i];
+        sumNeighbors += isReachable[i];
     }
-    //EV << self_id << " neighbor count: " << sum << endl;
+    //EV << self_id << " neighbor count: " << sumNeighbors << endl;
     if(par("firstInfected")){
         sendAll();
-    } else
+        emit(endTimeSignal, 0.0);
+        emit(hopSignal, infectionHop);
+    } else {
         scheduleClock();
+        emit(endTimeSignal, -1.0);
+        emit(hopSignal, -1);
+    }
     /*======statistics======*/
-    collisionSignal = registerSignal("collision");
+
+    emit(neighborsSignal, sumNeighbors);
 }
 
 void Node::colorNode(char* color){
@@ -100,12 +113,17 @@ void Node::handleMessage(cMessage *msg)
         collisionCheck = false;
         collisionOccurred = false;
         if(receivedInfection){
+            if (!hasValidMsg){
+                emit(endTimeSignal, simTime());
+                emit(hopSignal, infectionHop);
+            }
             hasValidMsg = true;
             double probability=par("sendingProbability");
             double limit=par("limitProbability");
             EV<<"INFETTO "<<self_id<<endl;
             if(probability<=limit){
                 sendAll();
+                /*====statistics====*/
                 goto end;
             }
             colorNode((char*)"blue");
@@ -118,6 +136,9 @@ void Node::handleMessage(cMessage *msg)
         if(!collisionCheck){
             collisionCheck = true;
             receivedInfection = true;
+            sscanf(msg->getName(), INFECTION_FRMT, &infectionHop);
+            infectionHop++;
+            EV << "received" << msg->getName() << " -> " << infectionHop << endl;
             colorNode((char*)"green");
         }
         else{
