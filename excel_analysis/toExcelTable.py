96f9de25959406b9953696a823f8d49fa0004798
giacomo.volpi99 <giacomo.volpi99@gmail.com>
2023-01-19 19:10:28 +0100

    Modify py script

--- excel_analysis/toExcelTable.py
+++ excel_analysis/toExcelTable.py
@@ -1,9 +1,9 @@
 import pandas as pd
-writer = pd.ExcelWriter('output.xlsx')   
+writer = pd.ExcelWriter('output2.xlsx')   
 workbook=writer.book
-worksheet=workbook.add_worksheet('Validation')
-writer.sheets['Validation'] = worksheet
-df=pd.read_csv("Untitled.csv")
+worksheet=workbook.add_worksheet('Result')
+writer.sheets['Result'] = worksheet
+df=pd.read_csv("200Nodes.csv")
 df=df[['run','name','vectime','vecvalue']]
 df=df.dropna(subset=['vectime'])
 df=df.reset_index()
@@ -14,14 +14,18 @@ valueDf=df['vecvalue'].str.split(' ', expand=True)
 finalDf = pd.DataFrame(columns=('run', 'name', 'vectime','vecvalue'))
 index=0
 run=0
+print(len(nameDf))
+print(len(timeDf.columns))
 for h in range(0,len(nameDf)):
-    for i in range(0,len(timeDf.columns)):
-        finalDf.loc[index]=[runDf[h],nameDf[h],timeDf[i][h],valueDf[i][h]]
-        index+=1
-        if(index==225):
-            finalDf['vectime']=pd.to_numeric(finalDf['vectime'])
-            finalDf['vecvalue']=pd.to_numeric(finalDf['vecvalue'])
-            finalDf.to_excel(writer,sheet_name='Validation',startcol=6*run , index=False)
-            run+=1
-            index=0      
+    if (nameDf[h]=="rateStatistic:vector"):
+        finalDf.loc[index]=[runDf[h],nameDf[h],timeDf[0][h],valueDf[0][h]]
+        finalDf['vectime']=pd.to_numeric(finalDf['vectime'])
+        finalDf['vecvalue']=pd.to_numeric(finalDf['vecvalue'])
+        finalDf.to_excel(writer,sheet_name='Result',startcol=6*run , index=False)
+        run+=1
+        index=0
+    else:
+        for i in range(0,len(timeDf.columns)):
+            finalDf.loc[index]=[runDf[h],nameDf[h],timeDf[i][h],valueDf[i][h]]
+            index+=1                  
 writer.save()
